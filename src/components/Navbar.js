4806afed00f99e0462f97f26a43fb2d532b9a7c0
Awais Azhar <razhar5214@bths.edu>
2022-03-13 22:53:45 -0400

    landing page background image, min-height changed to max-height to cover entire screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../styles/Navbar.css"
+import "../styles/navbar.css"
 import BackgroundImage from '../images/apartment-skyline.jpg'
 
 export default function Navbar() {
