59c5be524737669b26d468e3dbd42cb5c0793959
Awais <razhar5214@bths.edu>
2022-06-05 12:34:41 -0400

    removed node_modules folder

--- node_modules/react-google-places-autocomplete/build/utils/geocodeByAddress.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="google.maps" />
-declare const geocodeByAddress: (address: string) => Promise<google.maps.GeocoderResult[]>;
-export default geocodeByAddress;
--- node_modules/react-google-places-autocomplete/build/utils/geocodeByPlaceId.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="google.maps" />
-declare const geocodeByPlaceId: (placeId: string) => Promise<google.maps.GeocoderResult[]>;
-export default geocodeByPlaceId;
